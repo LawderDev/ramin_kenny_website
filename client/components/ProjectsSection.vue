0f9dcb3f2f3f08781654fdc3e3760157dc04a778
naby <naby@pop-os.localdomain>
2022-09-05 10:45:57 +0200

    🎨 add projects component

---
+++ client/components/ProjectsSection.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <h2 class="relative z-20 text-2xl flex items-center justify-center mb-16 font-bold">
+      Principaux Réalisations
+    </h2>
+    <swiper
+      :slides-per-view="slidesPerView"
+      :space-between="30"
+      :loop="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      :centered-slides="true"
+      :slide-to-clicked-slide="true"
+      :modules="[Pagination, Navigation]"
+      class="mySwipe w-[98vw] h-[33vh] overflow-visible"
+      @beforeInit="orientationChangeHandler"
+    >
+      <swiper-slide class="project-slides">
+        <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/project_IGDB.png" alt="IGDB">
+      </swiper-slide>
+      <swiper-slide class="project-slides">
+        <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/perso.png" alt="perso">
+      </swiper-slide>
+      <swiper-slide class="project-slides">
+        <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/project_IGDB.png" alt="IGDB">
+      </swiper-slide>
+      <swiper-slide class="project-slides">
+        <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2 " src="assets/imageindragon.png" alt="imageindragon">
+      </swiper-slide>
+      <swiper-slide class="project-slides">
+        <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/perso.png" alt="perso">
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Pagination, Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+// Import Swiper styles
+import 'swiper/css'
+
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { onUnmounted, ref } from '#imports'
+
+const slidesPerView = ref(-1)
+
+const orientationChangeHandler = () => {
+  slidesPerView.value = window.matchMedia('(orientation:portrait)').matches ? 2 : 5
+}
+
+if (process.client) {
+  orientationChangeHandler()
+  window.addEventListener('resize', orientationChangeHandler)
+}
+
+onUnmounted(() => {
+  if (process.client) {
+    window.removeEventListener('resize', orientationChangeHandler)
+  }
+})
+</script>
+
+<style>
+
+.mySwipe{
+  overflow: visible;
+}
+
+.project-slides:not(.swiper-slide-active){
+  @apply transition hover:ease-in-out hover:duration-150 hover:translate-y-1 hover:scale-110
+}
+
+.mySwipe .swiper-slide-active{
+  @apply transition ease-in-out duration-150 translate-y-1 scale-125 z-10
+}
+</style>
--- client/components/TheSections.vue
+++ client/components/TheSections.vue
@@ -10,89 +10,16 @@
         <skills-section />
       </div>
       <div class="section bg-[#152934] m-auto" data-anchor="page3">
-        <h2 class="relative z-20 text-2xl flex items-center justify-center mb-16 font-bold">
-          Principaux Réalisations
-        </h2>
-        <swiper
-          :slides-per-view="slidesPerView"
-          :space-between="30"
-          :loop="true"
-          :pagination="{
-            clickable: true,
-          }"
-          :navigation="true"
-          :centered-slides="true"
-          :slide-to-clicked-slide="true"
-          :modules="[Pagination, Navigation]"
-          class="mySwipe w-[98vw] h-[33vh] overflow-visible"
-          @beforeInit="orientationChangeHandler"
-        >
-          <swiper-slide class="project-slides">
-            <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/project_IGDB.png" alt="IGDB">
-          </swiper-slide>
-          <swiper-slide class="project-slides">
-            <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/perso.png" alt="perso">
-          </swiper-slide>
-          <swiper-slide class="project-slides">
-            <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/project_IGDB.png" alt="IGDB">
-          </swiper-slide>
-          <swiper-slide class="project-slides">
-            <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2 " src="assets/imageindragon.png" alt="imageindragon">
-          </swiper-slide>
-          <swiper-slide class="project-slides">
-            <img class="rounded-xl object-cover w-[60vw] h-[25vh] border-[#1B3543] border-2" src="assets/perso.png" alt="perso">
-          </swiper-slide>
-        </swiper>
-        <!-- <div class="container">
-          <input id="item-1" type="radio" name="slider" checked>
-          <input id="item-2" type="radio" name="slider">
-          <input id="item-3" type="radio" name="slider">
-          <div class="cards">
-            <label id="song-1" class="card" for="item-1">
-              <img class="rounded-xl object-cover w-full h-full border-[#1B3543] border-2" src="assets/project_IGDB.png" alt="IGDB">
-            </label>
-            <label id="song-2" class="card" for="item-2">
-              <img class="rounded-xl object-cover w-full h-full border-[#1B3543] border-2" src="assets/perso.png" alt="perso">
-            </label>
-            <label id="song-3" class="card" for="item-3">
-              <img class="rounded-xl object-cover w-full h-full border-[#1B3543] border-2 " src="assets/imageindragon.png" alt="imageindragon">
-            </label>
-          </div>
-        </div> -->
+        <projects-section />
       </div>
     </full-page>
   </ClientOnly>
 </template>
 
 <script lang="ts" setup>
-import { Pagination, Navigation } from 'swiper'
-import { Swiper, SwiperSlide } from 'swiper/vue'
 import HomeSection from '@/components/HomeSection.vue'
 import SkillsSection from '@/components/SkillsSection.vue'
-
-// Import Swiper styles
-import 'swiper/css'
-
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
-import { onUnmounted, ref } from '#imports'
-
-const slidesPerView = ref(-1)
-
-const orientationChangeHandler = () => {
-  slidesPerView.value = window.matchMedia('(orientation:portrait)').matches ? 2 : 5
-}
-
-if (process.client) {
-  orientationChangeHandler()
-  window.addEventListener('resize', orientationChangeHandler)
-}
-
-onUnmounted(() => {
-  if (process.client) {
-    window.removeEventListener('resize', orientationChangeHandler)
-  }
-})
+import ProjectsSection from '@/components/ProjectsSection.vue'
 </script>
 
 <style>
@@ -111,73 +38,7 @@ onUnmounted(() => {
     @apply top-[85vh] z-[1] max-w-[35vw] left-[62vw]
   }
 }
-
-.mySwipe{
-  overflow: visible;
-}
-
-.project-slides:not(.swiper-slide-active){
-  @apply transition hover:ease-in-out hover:duration-150 hover:translate-y-1 hover:scale-110
-}
-
-.mySwipe .swiper-slide-active{
-  @apply transition ease-in-out duration-150 translate-y-1 scale-125 z-10
-}
-
 .fp-watermark{
   visibility: hidden;
-}
-
-input[type=radio] {
-  display: none;
-}
-
-.card {
-  position: absolute;
-  width: 50vw;
-  height: 60vh;
-  left: 0;
-  right: 0;
-  margin: auto;
-  transition: transform .4s ease;
-  cursor: pointer;
-}
-
-.container {
-  width: 100%;
-  max-width: 800px;
-  max-height: 600px;
-  height: 100%;
-  transform-style: preserve-3d;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-}
-
-.cards {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  margin-bottom: 20px;
-}
-
-#item-1:checked ~ .cards #song-3, #item-2:checked ~ .cards #song-1, #item-3:checked ~ .cards #song-2 {
-  transform: translatex(-40%) scale(.8);
-  opacity: .4;
-  z-index: 0;
-}
-
-#item-1:checked ~ .cards #song-2, #item-2:checked ~ .cards #song-3, #item-3:checked ~ .cards #song-1 {
-  transform: translatex(40%) scale(.8);
-  opacity: .4;
-  z-index: 0;
-}
-
-#item-1:checked ~ .cards #song-1, #item-2:checked ~ .cards #song-2, #item-3:checked ~ .cards #song-3 {
-  transform: translatex(0) scale(1);
-  opacity: 1;
-  z-index: 1;
-  @apply transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300
 }
 </style>
