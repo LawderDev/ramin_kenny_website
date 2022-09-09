1486f77a119a89ea92f2cdaedfd8c1b6b76a5bae
naby <naby@pop-os.localdomain>
2022-09-09 12:23:31 +0200

    :iphone: responsive skills

--- client/components/SkillsSection.vue
+++ client/components/SkillsSection.vue
@@ -3,7 +3,7 @@
     <h2 class="relative z-20 text-2xl flex items-center justify-center mb-8 font-bold">
       Compétences
     </h2>
-    <div class="md:w-[90vw] md:h-auto w-[19rem] h-[40rem] m-auto md:ml-[9vw] z-10 relative">
+    <div class="landscape:w-[90vw] landscape:ml-[7.5vw] ml-[5.3vw] md:ml-[2.5vw] h-[40rem] m-auto z-10 relative">
       <swiper
         :slides-per-view="slidesPerView"
         :modules="[Pagination]"
@@ -13,109 +13,15 @@
         class="mySkillSwipe"
       >
         <swiper-slide>
-          <div class="skill-card">
-            <div class="pt-[4vh] pb-[2vh] xl:pt-[7vh] xl:pb-[5vh]">
-              <font-awesome-icon icon="paintbrush" class="text-[#7FD685] text-2xl xl:text-4xl" />
-            </div>
-            <h3 class="text-xl xl:text-2xl pb-4">
-              Front-end
-            </h3>
-            <h4 class="content-card  text-[#7FD685] pb-2">
-              Langage
-            </h4>
-            <ul class="content-card pb-4">
-              <li>Javascript</li>
-              <li>Typescript</li>
-              <li>CSS</li>
-            </ul>
-            <h4 class="content-card  text-[#7FD685] pb-2">
-              Framework et librairies
-            </h4>
-            <ul class="content-card ">
-              <li>Vue.js</li>
-              <li>Nuxt.js</li>
-              <li>TailwindCSS</li>
-              <li>Bootstrap</li>
-              <li>JQuery</li>
-            </ul>
-          </div>
+          <skill-card :title="'Front-end'" :content="frontSkills" />
         </swiper-slide>
 
         <swiper-slide>
-          <div class="skill-card">
-            <div class=" pt-[4vh] pb-[2vh] xl:pt-[7vh] xl:pb-[5vh]">
-              <font-awesome-icon icon="screwdriver-wrench" class="text-[#7FD685] text-3xl xl:text-4xl" />
-            </div>
-            <h3 class="text-xl xl:text-2xl pb-4">
-              Back-end
-            </h3>
-            <h4 class="content-card  text-[#7FD685] pb-2">
-              Langage
-            </h4>
-            <ul class="content-card  pb-4">
-              <li>Javascript</li>
-              <li>Typescript</li>
-              <li>PHP</li>
-            </ul>
-            <h4 class="content-card  text-[#7FD685] pb-2">
-              Framework et librairies
-            </h4>
-            <ul class="content-card  pb-4">
-              <li>Express.js</li>
-              <li>Laravel</li>
-              <li>Symfony</li>
-              <li>Spring Boot</li>
-            </ul>
-
-            <h4 class="content-card  text-[#7FD685] pb-2">
-              SGBD
-            </h4>
-            <ul class="content-card  pb-4">
-              <li>MySql</li>
-              <li>MongoDB</li>
-            </ul>
-
-            <h4 class="content-card  text-[#7FD685] pb-2">
-              CMS
-            </h4>
-            <ul class="content-card ">
-              <li>Strapi</li>
-            </ul>
-          </div>
+          <skill-card :title="'Back-end'" :content="backSkills" />
         </swiper-slide>
 
         <swiper-slide>
-          <div class="skill-card">
-            <div class=" pt-[4vh] pb-[2vh] xl:pt-[7vh] xl:pb-[5vh]">
-              <font-awesome-icon icon="plus" class="text-[#7FD685] text-3xl xl:text-4xl" />
-            </div>
-            <h3 class="text-xl xl:text-2xl pb-4">
-              Autres
-            </h3>
-            <h4 class="content-card text-[#7FD685] pb-2">
-              Langage
-            </h4>
-            <ul class="content-card pb-4">
-              <li>C</li>
-              <li>C#</li>
-              <li>Java</li>
-              <li>Kotlin</li>
-            </ul>
-            <h4 class="content-card text-[#7FD685] pb-2">
-              Framework et librairies
-            </h4>
-            <ul class="content-card  pb-4">
-              <li>Windows Form</li>
-              <li>WPF</li>
-            </ul>
-
-            <h4 class="content-card text-[#7FD685] pb-2">
-              Moteur de jeu
-            </h4>
-            <ul class="content-card xl:text-xl">
-              <li>Unity 3D</li>
-            </ul>
-          </div>
+          <skill-card :title="'Autres'" :content="otherSkills" />
         </swiper-slide>
       </swiper>
     </div>
@@ -125,13 +31,29 @@
 <script lang="ts" setup>
 import { Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
+import { SkillCard } from '#components'
 import { onUnmounted, ref } from '#imports'
+
+const frontSkills = [{ name: 'Langage', content: ['Javascript', 'Typescript', 'Css'] },
+  { name: 'Frameworks et librairies', content: ['Vue.js', 'Nuxt.js', 'TailwindCss', 'Bootstrap', 'JQuery'] }]
+
+const backSkills = [{ name: 'Langage', content: ['Javascript', 'Typescript', 'PHP'] },
+  { name: 'Frameworks et librairies', content: ['Express.js', 'Laravel', 'Symfony', 'Spring Boot'] },
+  { name: 'SGBD', content: ['MySql', 'MongoDB'] },
+  { name: 'CMS', content: ['Strapi'] }
+]
+
+const otherSkills = [{ name: 'Langage', content: ['C', 'C#', 'Java', 'Kotlin'] },
+  { name: 'Frameworks et librairies', content: ['Windows Form', 'WPF'] },
+  { name: 'SGBD', content: ['MySql', 'MongoDB'] },
+  { name: 'Moteur de jeu', content: ['Unity 3D'] }
+]
 
 const slidesPerView = ref(-1)
 
 const orientationChangeHandler = () => {
   slidesPerView.value = window.matchMedia('(orientation:portrait)').matches && window.matchMedia('(max-width: 767px)').matches ? 1 : 3
-  if (window.matchMedia('(orientation:portrait)').matches && window.matchMedia('(min-width: 768px)').matches) { slidesPerView.value = 2 }
+  if (window.matchMedia('(orientation:portrait)').matches && window.matchMedia('(min-width: 768px)').matches) { slidesPerView.value = 3 }
 }
 orientationChangeHandler()
 
@@ -146,12 +68,9 @@ onUnmounted(() => {
 })
 </script>
 
-<style scoped>
-.skill-card{
-  @apply w-[19rem] h-[40rem] md:h-[40rem] md:w-[17rem] lg:w-[19rem] lg:h-[40rem] xl:w-[27rem] xl:h-[44.5rem] bg-[#1B3543] rounded-2xl z-20 flex flex-col items-center text-center border-[#152934] transition-colors hover:border-[#7FD685] border-2
+<style>
+.mySkillSwipe .swiper-slide{
+  @apply landscape:w-[25vw]
 }
 
-.content-card{
-  @apply text-lg xl:text-xl
-}
 </style>
